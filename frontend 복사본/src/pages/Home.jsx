d598695aff76ae0069daf6c0fd46ff2c08afe3b6
luminox93 <memoji1236@gmail.com>
2024-05-16 08:34:48 +0900

    delete: 복사본 삭제

--- frontend 복사본/src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-import PageLayout from '../layouts/PageLayout';
-import Header from '../layouts/Header';
-
-const Home = () => {
-  return <PageLayout header={<Header />}>Home</PageLayout>;
-};
-
-export default Home;
